c53d452079d111bc0c9511c6f987b4085e24e6ee
cdhenley219 <cdhenley219@hotmail.com>
2023-01-20 18:22:37 -0500

    Updated to use BaseImage width and height

--- src/components/BaseImage/BaseImage.js
+++ src/components/BaseImage/BaseImage.js
@@ -2,8 +2,8 @@ import './base-image.css';
 
 const baseUrl = 'https://ik.imagekit.io/ptbmshrys';
 
-const BaseImage = ({name}) => (
-    <img src={`${baseUrl}/${name}.jpg`} alt={name} className="base-image"/>
+const BaseImage = ({name, width, height}) => (
+    <img src={`${baseUrl}/${name}.jpg`} alt={name} className="base-image" style={{width: width || '100%', height: height || '100%'}}/>
 );
 
 export default BaseImage;
--- src/components/FillInTheBlank/FillInTheBlank.js
+++ src/components/FillInTheBlank/FillInTheBlank.js
@@ -4,7 +4,7 @@ import './fill-in-the-blank.css';
 const FillInTheBlank = ({choice, value, onAnswerChange=()=>{} }) => {
     console.log(value);
    return (<div className="survey-fill-in-blank">
-        <QuestionText text={choice.question.text}/>
+        <QuestionText text={choice.question.text} mediaSrc={choice.question.mediaSrc} mediaSrcWidth={choice.question.mediaSrcWidth} mediaSrcHeight={choice.question.mediaSrcHeight}/>
         <input id="id" type="text" value={value} onChange={onAnswerChange}/>
     </div>)
 };
