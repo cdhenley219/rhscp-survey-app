9e73874a97b0ef8de9917b86381a87e9ddc192e6
techieann <tosha114@yahoo.com>
2023-01-25 22:36:11 -0500

    Update Base Video.js
    Update Base Video.js - removed iframe and added video tag

--- src/components/BaseVideo/BaseVideo.js
+++ src/components/BaseVideo/BaseVideo.js
@@ -5,10 +5,17 @@ const baseUrl = 'https://ik.imagekit.io/ptbmshrys';
 
 const VideoImage = ({name, width, height}) => 
 (
-    <iframe title='Video' width="420" height="345" 
+ /*   <iframe title='Video' width="420" height="345" 
     src={`${baseUrl}/${name}.mov`} alt={name} className="base-video" 
     style={{width: width || '100%', height: height || '100%'}}>
     </iframe>
+*/
+ 
+
+<video width="750" height="500" controls >
+<source src={`${baseUrl}/${name}.mov`} />
+</video>
+
 );
    
 export default VideoImage;
