0457e167b632ea410a4c941ba50630d620cdf8d2
cdhenley219 <cdhenley219@hotmail.com>
2023-01-28 23:40:52 -0500

    updated schema

--- src/util/api.js
+++ src/util/api.js
@@ -18,8 +18,7 @@ const getSurvey = async (surveyId) => {
 
 const startSurveySession = async (surveyId) => {
     const body = JSON.stringify({
-        language: 'EN',
-        embeddedData:{}
+        language: 'EN'
     });    
 
     try {
@@ -39,26 +38,183 @@ const startSurveySession = async (surveyId) => {
 const updateSurveySession = async (surveyId, sessionId, payload) => {
     // TODO: pass language, embeddedData, recipientId, and distributedId
 
+    // const body = JSON.stringify({
+    //     advance: true,
+    //     embeddedData: {
+    //         "edKey": "edValue",
+    //         "customerName": "Kyle"
+    //     },
+    //     //close: false,
+    //     responses: {
+    //         "QID1715170247": {
+    //             "1": {
+    //               "selected": true,
+    //               "text": "string"
+    //             },
+    //             "2": {
+    //               "selected": false,
+    //               "text": "string"
+    //             }          
+    //         }/*,
+    //         'QID1715170248': 'baseball'*/
+    //     }
+    // });
+
     const body = JSON.stringify({
-        advance: true,
-        embeddedData: {},
-        //close: true,
+        close: true,
+        //close: false,
         responses: {
             "QID1715170247": {
                 "1": {
                   "selected": true,
-                  text: "Yes"
                 },
-          
+                "2": {
+                  "selected": true,
+                }
+            },
+            'QID1715170248': 'baseball',
+            'QID1715170249': 'test',
+            "QID1715170250": {
+                "1": {
+                  "selected": true,
+                },
                 "2": {
                   "selected": false,
-                  text: "No"
-                }          
+                },
+                "3": {
+                    "selected": false,
+                },
+                "4": {
+                    "selected": false,
+                }
             },
-            'QID1715170248': 'baseball'
+            "QID1715170251": {
+                "1": {
+                  "selected": false,
+                },
+                "2": {
+                  "selected": true,
+                },
+                "3": {
+                    "selected": false,
+                },
+                "4": {
+                    "selected": false,
+                }
+            },
+            "QID1715170252": {
+                "1": {
+                  "selected": false,
+                },
+                "2": {
+                  "selected": true,
+                },
+                "3": {
+                    "selected": false,
+                },
+                "4": {
+                    "selected": false,
+                }
+            },
+            "QID1715170253": {
+                "1": {
+                  "selected": false,
+                },
+                "2": {
+                  "selected": true,
+                },
+                "3": {
+                    "selected": false,
+                },
+                "4": {
+                    "selected": false,
+                },
+                "5": {
+                    "selected": false,
+                }
+            },
+            "QID1715170254": {
+                "1": {
+                  "selected": false,
+                },
+                "2": {
+                  "selected": true,
+                },
+                "3": {
+                    "selected": false,
+                },
+                "4": {
+                    "selected": false,
+                },
+                "5": {
+                    "selected": false,
+                }
+            },
+            "QID1715170255": {
+                "1": {
+                  "selected": false,
+                },
+                "2": {
+                  "selected": true,
+                },
+                "3": {
+                    "selected": false,
+                },
+                "4": {
+                    "selected": false,
+                },
+                "5": {
+                    "selected": false,
+                }
+            },
+            "QID1715170256": {
+                "1": {
+                  "selected": false,
+                },
+                "2": {
+                  "selected": true,
+                },
+                "3": {
+                    "selected": false,
+                }
+            },
+            "QID1715170257": {
+                "1": {
+                  "selected": false,
+                },
+                "2": {
+                  "selected": true,
+                },
+                "3": {
+                    "selected": false,
+                }
+            },
+            "QID1715170258": {
+                "1": {
+                  "selected": false,
+                },
+                "2": {
+                  "selected": true,
+                },
+                "3": {
+                    "selected": false,
+                }
+            },
+            "QID1715170259": {
+                "1": {
+                  "selected": false,
+                },
+                "2": {
+                  "selected": true,
+                },
+                "3": {
+                    "selected": false,
+                }
+            }
         }
     });
 
+    //  "body": "{\"advance\":true,\"embeddedData\":{\"edKey\":\"edValue\",\"customerName\":\"Kyle\"},\"responses\":{\"property1\":{\"property1\":{\"selected\":true,\"text\":\"string\"},\"property2\":{\"selected\":true,\"text\":\"string\"}},\"property2\":{\"property1\":{\"selected\":true,\"text\":\"string\"},\"property2\":{\"selected\":true,\"text\":\"string\"}}},\"close\":true}"
     try {
         const response = await fetch(`${URL_PREFIX}/API/v3/surveys/${surveyId}/sessions/${sessionId}`, {
             method: 'POST',
