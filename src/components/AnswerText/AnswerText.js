00a4317fd26a0cb6a5420d60000daf4595ba529c
cdhenley219 <cdhenley219@hotmail.com>
2023-02-01 12:54:20 -0500

    fixed linting issue

--- src/components/AnswerText/AnswerText.js
+++ src/components/AnswerText/AnswerText.js
@@ -1,4 +1,3 @@
-import { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import BaseImage from '../BaseImage/BaseImage';
 import BaseVideo from '../BaseVideo/BaseVideo';
