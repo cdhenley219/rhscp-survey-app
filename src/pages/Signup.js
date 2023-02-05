43ce0b369edf87476fd8a8a6e7e81b0d4aa631cd
cdhenley219 <cdhenley219@hotmail.com>
2023-02-05 18:52:02 -0500

    signup updates #2

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -15,8 +15,6 @@ const [fname, setFirstName] = useState('');
 const [lname, setLastName] = useState('');
 const [grade, setGrade] = useState('');
 const [birthdate, setBirthDate] = useState('');
-//const [email, setEmail] = useState('');
-//const [password, setPassword] = useState('');
 
 // States for checking the errors
 const [submitted, setSubmitted] = useState(false);
@@ -25,39 +23,28 @@ const [gradeInvalid, setGradeInvalid] = useState(false);
 
 // Handling the First Name change
 const handleFirstName = (e) => {
-setFirstName(e.target.value);
-setSubmitted(false);
+    setFirstName(e.target.value);
+    setSubmitted(false);
 };
 
 // Handling the Last Name change
 const handleLastName = (e) => {
-setLastName(e.target.value);
-setSubmitted(false);
+    setLastName(e.target.value);
+    setSubmitted(false);
 };
 
 // Handling the Grade change
 const handleGrade = (e) => {
-setGrade(e.target.value);
-setSubmitted(false);
+    setGrade(e.target.value);
+    setSubmitted(false);
 };
 
 // Handling the Birthdate change
 const handleBirthDate = (e) => {
-setBirthDate(e.target.value);
-setSubmitted(false);
+    setBirthDate(e.target.value);
+    setSubmitted(false);
 };
 
-// // Handling the email change
-// const handleEmail = (e) => {
-// setEmail(e.target.value);
-// setSubmitted(false);
-// };
-
-// // Handling the password change
-// const handlePassword = (e) => {
-// setPassword(e.target.value);
-// setSubmitted(false);
-// };
 
 const isGradeSelectionValid = grade => {
     if (grade === 'Kindergarten' || grade === '1st' || grade === '2nd') {
@@ -86,9 +73,7 @@ const getUniqueId = () => {
 // Handling the form submission
 const handleSubmit = (e) => {
     e.preventDefault();
-    setError(false);
-    setGradeInvalid(false);
-    if (fname === '' || lname === '' || grade === '' || birthdate === ''  /*|| email === '' || password === ''*/) {
+    if (fname === '' || lname === '' || grade === '' || birthdate === '') {
         setError(true);
     } 
     else if (!isGradeSelectionValid(grade)) {
@@ -97,109 +82,68 @@ const handleSubmit = (e) => {
     else {
         setSubmitted(true);
         setError(false);
+        setGradeInvalid(false);
     }
     localStorage.setItem('survUid', getUniqueId());
     navigate(`/survey/${surveyId}`);
-   // window.location.href = `/survey/${surveyId}`;
-
-// e.preventDefault();
-// if (fname === '' || lname === '' || email === '' || password === '') {
-// setError(true);
-// } else {
-// setSubmitted(true);
-// setError(false);
-// }
-};
-
-// Showing success message
-const successMessage = () => {
-    return (
-        <div
-            className="success"
-            style={{
-            display: submitted ? '' : 'none',
-            }}>
-            <h2>User {fname} {lname} successfully registered!!</h2>
-        </div>
-        );
-    
-// return (
-// <div
-// className="success"
-// style={{
-// display: submitted ? '' : 'none',
-// }}>
-// <h2>User {fname} {lname} successfully registered!!</h2>
-// </div>
-// );
 };
 
 // Showing error message if error is true
-const errorMessage = () => {
-return (
-<div
-className="error"
-style={{
-display: error ? '' : 'none',
-}}>
-<h1>Please enter all the fields</h1>
-</div>
+const errorMessage = () => (
+    <>
+   { error && <div className="error">
+        <h1>Please enter all the fields</h1> 
+    </div> 
+    }
+
+    { gradeInvalid && <div className="error">
+        <h1>Invalid Grade</h1>
+    </div>
+    }
+    </>
 );
-};
 
 
 return (
 <div className="signupform">
-<div>
+    <div>
 
-<div className='signupformHeader'>
-<Logo />
-<h1>Online Assessments Signup</h1>
-</div>
-</div>
+        <div className='signupformHeader'>
+            <Logo />
+                <h1>Online Assessments Signup</h1>
+        </div>
+    </div>
 
 {/* Calling to the methods */}
-<div className="messages">
-{errorMessage()}
-{successMessage()}
-</div>
+    <div className="messages">
+        {errorMessage()}
+    </div>
 
-<form>
-{/* Labels and inputs for form data */}
-<p>
-<label className="label">First Name</label>
-<input onChange={handleFirstName} className="input"
-value={fname} type="text" />
+    <form>
+    {/* Labels and inputs for form data */}
+        <div>
+            <label className="label">First Name</label>
+            <input onChange={handleFirstName} className="input" value={fname} type="text" />
 
-<label className="label">Last Name</label>
-<input onChange={handleLastName} className="input"
-value={lname} type="text" />
+            <label className="label">Last Name</label>
+            <input onChange={handleLastName} className="input" value={lname} type="text" />
 
-<label className="label">Grade</label>
-<select onChange={handleGrade} className="input" value={grade} type="number" >
-    <option key="-1"></option>
-    {grades.map ((grade, index) => (
-        <>
-        <option key={index}>{grade}</option>
-        </>
-    ))}
-</select>
+            <label className="label">Grade</label>
+            <select onChange={handleGrade} className="input" value={grade} type="number" >
+                <>
+                <option key="-1"></option>
+                {grades.map ((grade, index) => (
+                    <option key={index}>{grade}</option>
+                ))}
+                </>
+            </select>
 
+            <label className="label">Date of Birth</label>
+            <input onChange={handleBirthDate} className="input" value={birthdate} type="date" />
+        </div>
 
-<label className="label">Date of Birth</label>
-<input onChange={handleBirthDate} className="input"
-value={birthdate} type="date" />
-
-{/* <label className="label">Email</label>
-<input onChange={handleEmail} className="input"
-value={email} type="email" />
-
-<label className="label">Password</label>
-<input onChange={handlePassword} className="input"
-value={password} type="password" /> */}
-</p>
-<button onClick={handleSubmit} className="btn" type="submit">Go to Assessment</button>
-</form>
+        <button onClick={handleSubmit} className="btn" type="submit">Go to Assessment</button>
+    </form>
 </div>
 );
 }
